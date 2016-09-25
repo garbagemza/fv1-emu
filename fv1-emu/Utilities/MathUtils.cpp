eb43b2f110a5895475b08a00aa5f8622b557291d
garbagemza <garbagemza@gmail.com>
2016-09-25 10:53:05 -0300

    Preparing compilation on VS2010

--- fv1-emu/Utilities/MathUtils.cpp
+++ fv1-emu/Utilities/MathUtils.cpp
@@ -1,6 +1,13 @@
 #include "MathUtils.h"
 #include <math.h>
 
+#ifdef _MSC_VER
+#if _MSC_VER < 1800
+double log2(double acc) {
+	return log(acc) / log(2.0);
+}
+#endif
+#endif
 double fv1log2(double acc) {
 	if (acc < 0.00001526)
 		acc = 0.00001526;
